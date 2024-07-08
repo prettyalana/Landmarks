aa6d5614622b05f0dd423c27a2a790875e266601
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-07-08 15:28:12 -0500

    Building lists and navigation Created a landmark model, created row view, set up navigation between list and detail, and passed data into child views

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -7,19 +7,29 @@
 
 import SwiftUI
 
+
 struct LandmarkRow: View {
     var landmark: Landmark
-    
+
+
     var body: some View {
         HStack {
             landmark.image
                 .resizable()
                 .frame(width: 50, height: 50)
             Text(landmark.name)
+
+
+            Spacer()
         }
     }
 }
 
+
 #Preview {
-    LandmarkRow(landmark: landmarks[0])
+    Group {
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+    }
 }
+
